d5ec313c253d0b105a4f0d1008288586c2e001e1
KentHall303 <133552538+KentHall303@users.noreply.github.com>
2025-11-14 22:22:25 -0700

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: false,
+  },
 });
